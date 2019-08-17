93bb2d8f53ac19c0d835b6f133bc3ee992b15582
Matt K <matthewselkirk@gmail.com>
2019-08-17 14:46:36 +0200

    script now checks for paths before setting vhhost folder config

--- HyperV/NewHyperVConfiguration.ps1
+++ HyperV/NewHyperVConfiguration.ps1
@@ -1,21 +1,59 @@
+[CmdletBinding()]
+# Parameter help description
+Param(
+    [Parameter(Mandatory=$true)]   
+    [String]$VMHDsDefaultPath,
+    [Parameter(Mandatory=$true)]   
+    [String]$VMsDefaultPath
+  
+)
 
-[String]$VDPath = ""
-[String]$VMpath = ""
 
+Function checkPath{
+param (
+[Parameter(Mandatory=$true)]
+[String]$FolderPath
+)
+
+try{
+if( !(Test-Path -Path $FolderPath)){
+
+    Write-Host "Folder does not exist, do you want to create the folder in path $($FolderPath)?" -ForegroundColor Green
+
+    New-Item -Path $FolderPath -ItemType "Directory"
+}
+else{
+
+    Write-Host "Folder path exists, continuing with script" -ForegroundColor Green
+
+}
+
+}catch{
+    Write-Host "Error checking folder path. Error is: $($_)."
+    Pause
+    Exit
+    }
+}
 
 try{
 
-    Write-Host "Setting default VM directory path" -ForegroundColor Green
+    Write-Host "Setting default VM HardDisk directory path to $($VMHDsDefaultPath) and VM Configuration Path to $($VMsDefaultPath)" -ForegroundColor Green
 
+    #check paths
+    checkPath -FolderPath $VMHDsDefaultPath
+    checkPath -FolderPath $VMsDefaultPath   
+    
     Import-Module -Name Hyper-V
 
-    Set-VMHost -VirtualHardDiskPath $VDPath -VirtualMachinePath $VMpath
+    Set-VMHost -VirtualHardDiskPath $VMHDsDefaultPath -VirtualMachinePath $VMsDefaultPath
 
 }
 catch{
 
-    write-host "Error configuraing Hyper-V host $($_)." -ForegroundColor Red
+    Write-Host "Error configuraing Hyper-V host $($_)." -ForegroundColor Red
     Pause
     Exit-PSHostProcess
 
 }
+
+
